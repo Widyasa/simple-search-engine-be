6cddebaf46aa5e7704e6906c07aad92e879ccf7f
widya Yasa <widyarusmananda15@gmail.com>
2024-10-04 00:42:05 +0800

    add stopword

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -2,6 +2,8 @@ import TfIdf from 'tf-idf-search'
 import {blogs} from "../datas/blog.js";
 import {sendResponse} from "../utils/response.js";
 import natural from "natural";
+import {removeStopwords} from "stopword";
+
 const tf_idf = new TfIdf()
 // Menggunakan PorterStemmer dari natural
 const stemmer = natural.PorterStemmer;
@@ -10,38 +12,40 @@ export const getBlogs = async (req, res) => {
     const query = req.query.search;
     try {
         if (!query) {
-            return sendResponse(res, true, blogs, 'All Data')
+            return sendResponse(res, true, blogs, 'All Data');
         }
 
-        // Lakukan stemming pada query
-        const stemmedQuery = query.split(' ').map(word => stemmer.stem(word)).join(' ');
+        // Lakukan stemming pada query dan hapus stopwords
+        const stemmedQuery = removeStopwords(query.split(' '))
+            .map(word => stemmer.stem(word))
+            .join(' ');
 
         const blogContent = blogs.map((blog) => {
-            const stemmedTitle = blog.title.replace(/[^a-zA-Z0-9 ]/g, "").toLowerCase().split(' ').map(word => stemmer.stem(word)).join(' ');
-            const stemmedAuthor = blog.author.replace(/[^a-zA-Z0-9 ]/g, "").toLowerCase().split(' ').map(word => stemmer.stem(word)).join(' ');
-            const stemmedContent = blog.content.replace(/[^a-zA-Z0-9 ]/g, "").toLowerCase().split(' ').map(word => stemmer.stem(word)).join(' ');
-            return `${stemmedTitle} - ${stemmedAuthor} - ${stemmedContent} - ${blog.date}`;
+            const combinedText = `${blog.title} ${blog.author} ${blog.content}`.replace(/[^a-zA-Z0-9 ]/g, "").toLowerCase();
+            return removeStopwords(combinedText.split(' '))
+                .map(word => stemmer.stem(word))
+                .join(' ');
         });
 
+        // Tambahkan setiap dokumen ke corpus
         let corpus = tf_idf.createCorpusFromStringArray(
             blogContent
         )
 
-        // Gunakan query yang sudah di-stem
-        let search = tf_idf.rankDocumentsByQuery(stemmedQuery)
+        // Gunakan query yang sudah di-stem dan di-filter stopwords
+        let searchResults = tf_idf.rankDocumentsByQuery(stemmedQuery);
 
-        let result = []
-        search.forEach(item => {
-            if (item.similarityIndex > 0) {
-                const blog = blogs[item.index];
-                result.push({
+        const result = searchResults.filter((result) => result.index < blogs.length)
+            .map((result) => {
+                const blog = blogs[result.index];
+                return {
                     ...blog,
-                    score: item.similarityIndex,
-                })
-            }
-        })
+                    score: result.similarityIndex,
+                    token: result.document
+                };
+            });
 
-        return sendResponse(res, true, result, 'Articles ranked by TF-IDF')
+        return sendResponse(res, true, result, 'Articles ranked by TF-IDF');
     } catch (e) {
         console.error('Error fetching articles:', e);
         sendResponse(res, false, null, 'Failed to fetch articles', 500);
